9b0588398d1045b3d507f5157ad1f40d7c7e699c
bharats <sonawanebharat23@gmail.com>
2021-06-16 20:13:43 +0530

    final changes

--- app/src/main/java/com/example/movieticketbookingassignment/CreateAccountActivity.java
+++ app/src/main/java/com/example/movieticketbookingassignment/CreateAccountActivity.java
@@ -32,6 +32,9 @@ public class CreateAccountActivity extends AppCompatActivity {
 
                 String user = usernameEdit.getText().toString();
                 String email = emailEdit.getText().toString();
+
+
+
                 String password = passwordEdit.getText().toString();
 
 
